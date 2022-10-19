183d755fdda23db0b46cc85927de51588aad0d57
Faith74 <faithkimaniii948@gmail.com>
2022-10-19 05:47:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabets a to z
+ * return 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+}
